86ec173cf10e1f2c729e57b15f4bdb8dbc1fa647
Martin <martin.fetzel@get-the-solution.net>
2024-03-24 19:48:00 +0100

    Set up Windows Application Packaging Project (#9)
    Adds app package for the ms store to pipeline
    - [x] x86
    - [ ] x64
    
    Add a welcome page
    
    - showing the current config
    - points to the config such that the user experienced users can change their settings
    - exits the app such that the user doesnt need the taskmanager anymore
    
    ---------------------------------
    
    * Set up Windows Application Packaging Project
    
    * [Ignore("Skipping this unit test in the pipeline")]
    
    * # Add  MsBuild to the PATH: https://github.com/microsoft/setup-msbuild
        - name: Setup MSBuild.exe
          uses: microsoft/setup-msbuild@v1.0.0
    
    * - name: Add msbuild to PATH
      uses: microsoft/setup-msbuild@v2
    
    * /p:TargetFramework=net8.0-windows /p:Runtimeidentifier=win-$env:TargetPlatform
    
    * Create an X509Certificate2 object from the byte array
    
    * certFilePath
    
    * # if: github.event_name != 'pull_request' && env.Wap_build == 'true'
    
    * dir ${{env.AppxPackageDir}}
    
    * AppxPackageDir
    
    * ${{env.AppxPackageDir}}
    
    * restore
    
    * Configuration
    
    * Release
    
    * ${{ matrix.configuration }}
    
    * /p:Runtimeidentifier=win-$env:TargetPlatform `
    
    * #
    
    * /t:Restore `
    
    * dir
    
    * dir mkdir ${{env.AppxPackageDir}}
    
    * dir ${{env.AppxPackageDir}}
    
    * /t:Restore;Build `
    
    * /t:"Restore;Build" `
    
    * Runtimeidentifier
    
    * x86
    
    * ${{ env.AppxPackageDir }}
    
    * /t:"Restore;Build" `
    
    * /p:AppxBundlePlatforms="${{matrix.targetplatform}}" `
    
    * ${{matrix.targetplatform}}
    
    * fix idendity
    
    * Update manifest version
    
    * if: github.event_name != 'pull_request' && env.Wap_build == 'true'
    
    * x64
    
    * # Only build the wapproj for x86
    
    * add Welcome page
    
    * if: env.Wap_build == 'true' && matrix.targetplatform != 'x64'

---
+++ KeyboardMouseWin/Command.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+
+namespace KeyboardMouseWin;
+
+public class Command(Func<object?, Task> onCommandFunc, Func<object?, bool> onCancelFunc) : ICommand
+{
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+        => onCancelFunc.Invoke(parameter);
+
+
+    public void Execute(object? parameter)
+    {
+        _ = onCommandFunc.Invoke(parameter);
+    }
+}
